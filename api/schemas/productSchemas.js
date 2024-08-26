abfe5fd566ae355b13bb4ab243d6210aeac430b0
Christian Arias <chris.29.01.44@gmail.com>
2024-08-26 08:52:14 -0500

    error product

--- api/schemas/productSchemas.js
+++ api/schemas/productSchemas.js
@@ -41,9 +41,6 @@ const queryProductSchema = Joi.object({
   offset,
   price,
   price_min,
-  price_max: price_max.when('price_min', {
-    is: Joi.number().integer(),
-    then: Joi.required()
-  })
+  price_max
 })
 module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema }
