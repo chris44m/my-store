e49f49198eb4505660e25794d3cd7f3086e13833
Christian Arias <chris.29.01.44@gmail.com>
2024-08-26 02:04:10 -0500

    fixing

--- api/libs/postgresPool.js
+++ api/libs/postgresPool.js
@@ -2,23 +2,23 @@ const { Pool } = require('pg');
 
 const { config } = require('../config/config')
 
-let URI = '';
-
+const options = {};
 if (config.isProd) {
+  options.connectionString = config.dbUrl;
+  options.ssl = {
+    rejectUnauthorized: false
+  }
   URI = config.dbUrl;
 }
 else {
   const USER = encodeURIComponent(config.dbUser);
   const PASSWORD = encodeURIComponent(config.dbPassword);
-  URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
+  const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+  options.connectionString = URI;
 }
 
 
-
-const pool = new Pool({
-  connectionString: URI
-})
+const pool = new Pool(options)
 
 
 module.exports = pool;
