45cce7e6972215389d195bd7b64a296bcc6eb044
Christian Arias <chris.29.01.44@gmail.com>
2024-08-26 02:23:34 -0500

    migrations fix

--- db/migrations/20240822170759-add-role.js
+++ db/migrations/20240822170759-add-role.js
@@ -1,17 +1,16 @@
 'use strict';
 const { USER_TABLE } = require('./../models/userModel')
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn(USER_TABLE, 'role', {
-      role: {
-        allowNull: false,
-        type: DataTypes.STRING,
-        defaultValue: 'customer'
-      },
+      allowNull: false,
+      type: DataTypes.STRING,
+      defaultValue: 'customer'
+
     })
   },
 
